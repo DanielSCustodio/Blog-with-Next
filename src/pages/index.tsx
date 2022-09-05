fcbb529ba527bf87d66229ea8aaf07290701d505
Daniel Custodio <play.daniel@hotmail.com>
2022-09-05 19:20:45 -0300

    feat: Fake API with client side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
-import styles from '../styles/home.module.sass'
-export default function Home() {
+import React from "react";
+interface Post {
+  id: string;
+  title: string;
+}
+
+export default function Home () {
+  const [ posts, setPosts ] = React.useState<Post[]>( [] );
+  React.useEffect( () => {
+    fetch( 'http://localhost:3333/posts' )
+      .then( ( response ) => {response.json()
+      .then( ( data ) => { setPosts( data ) } );
+      })
+  }, [] );
+
+  console.log(posts);
+  
   return (
     <div>
-      <h1>Olá <span>Dev</span>!</h1>
+      <h1>Posts</h1>
+      <ul>
+        {posts && posts.map(({title, id})=>(
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
