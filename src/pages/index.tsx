e95637212a376512c8e196e6052ed170a4114e78
Daniel Custodio <play.daniel@hotmail.com>
2022-09-05 19:52:03 -0300

    feat: Fake API with server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,44 @@
+import { GetServerSideProps } from "next";
 import React from "react";
 interface Post {
   id: string;
   title: string;
 }
 
-export default function Home () {
-  const [ posts, setPosts ] = React.useState<Post[]>( [] );
+interface HomeProps {
+  posts: Post[];
+}
+
+export default function Home ({posts}) {
+/*   const [ posts, setPosts ] = React.useState<Post[]>( [] );
   React.useEffect( () => {
     fetch( 'http://localhost:3333/posts' )
-      .then( ( response ) => {response.json()
-      .then( ( data ) => { setPosts( data ) } );
-      })
-  }, [] );
+      .then( ( response ) => {
+        response.json()
+        .then( ( data ) => { setPosts( data ) } );
+      } )
+  }, [] ); */
 
-  console.log(posts);
-  
+  console.log( posts );
+
   return (
     <div>
       <h1>Posts</h1>
       <ul>
-        {posts && posts.map(({title, id})=>(
-          <li key={id}>{title}</li>
-        ))}
+        { posts && posts.map( ( { title, id } ) => (
+          <li key={ id }>{ title }</li>
+        ) ) }
       </ul>
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch( 'http://localhost:3333/posts' );
+  const posts = await response.json();
+  return {
+    props: {
+      posts,
+    }
+  }
 }
