326b61de17eff859ee97248fc87084ec9f9dad65
Daniel Custodio <play.daniel@hotmail.com>
2022-09-05 22:00:31 -0300

    feat: Fake API with static site generation

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,34 @@
-export default function Posts() {
+import { GetStaticProps } from "next";
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts ( { posts }:PostsProps ) {
   return (
     <div>
-      <h1>Posts</h1>
+      <h1>Listagem de Posts</h1>
+      <ul>
+        { posts && posts.map( ( { title, id } ) => (
+          <li key={ id }>{ title }</li>
+        ) ) }
+      </ul>
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps<PostsProps> = async () => {
+  const response = await fetch( 'http://localhost:3333/posts' );
+  const posts = await response.json();
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 5, //Segundos - A página é regerada a cada 5 segundos, buscando novas informaçõea da API
+  }
 }
